1e8f45e5917121ea2e0af242b9f18c52d3c4e66f
jbhatab <jbhatab@gmail.com>
2014-11-29 12:14:15 -0600

    cleaned up app

---
+++ source/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require 'jquery/dist/jquery'
+
+$('#custom-box').click(function(){
+  if($(this).hasClass('grow')) {
+    $(this).removeClass('grow')
+  } else {
+    $(this).addClass('grow')
+  }
+})
